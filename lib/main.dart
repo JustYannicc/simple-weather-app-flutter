fc459fefa32db5bd6ffd49e5ce8b623870fc7d04
JustYannicc <52761674+JustYannicc@users.noreply.github.com>
2023-04-30 11:01:17 +0200

    Mapbox issue

--- lib/main.dart
+++ lib/main.dart
@@ -106,6 +106,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   String formatedlocation = '';
+  double longitude = 0;
+  double latitude = 0;
 
   formatlocation(pos) {
     final String input = pos.toString();
@@ -116,6 +118,8 @@ class _MyHomePageState extends State<MyHomePage> {
     final formatedlocation = '$latitude,$longitude';
     setState(() {
       this.formatedlocation = formatedlocation;
+      this.longitude = longitude;
+      this.latitude = latitude;
     });
     return formatedlocation;
   }
@@ -172,15 +176,6 @@ class _MyHomePageState extends State<MyHomePage> {
     await fetchcurrentweather(formatedlocation);
   }
 
-  MapboxMap map = MapboxMap(
-    accessToken: 'pk.eyJ1IjoianVzdHlhbm5pY2MiLCJhIjoiY2xoMjhmY2xiMWIweTNxcnptemcxYWVsOCJ9.nNpMduoTVhCHkvecc0ffCw',
-    onMapCreated: (MapboxMapController controller) {},
-    initialCameraPosition: CameraPosition(
-      target: LatLng(formatedlocation),
-      zoom: 12.0,
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -195,48 +190,56 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: MapboxMap(
-        accessToken: 'pk.eyJ1IjoianVzdHlhbm5pY2MiLCJhIjoiY2xoMjhmY2xiMWIweTNxcnptemcxYWVsOCJ9.nNpMduoTVhCHkvecc0ffCw',
-          onMapCreated: (controller) {
-            // Add any initialization code here
-          },
-          options: MapboxMapOptions(
-            styleString: 'mapbox://styles/MAPBOX_USERNAME/STYLE_ID',
-            cameraPosition: CameraPosition(
-              target: LatLng(37.7749, -122.4194),
-              zoom: 12.0,
-                ),
+      body: Stack(
+        children: [
+          Container(
+            child: MapboxMap(
+              accessToken: 'pk.eyJ1IjoianVzdHlhbm5pY2MiLCJhIjoiY2xoMjhmY2xiMWIweTNxcnptemcxYWVsOCJ9.nNpMduoTVhCHkvecc0ffCw',
+              onMapCreated: (controller) {
+                // Add any initialization code here
+              },
+              cameraTargetBounds: CameraTargetBounds.unbounded,
+              styleString: 'mapbox://styles/justyannicc/clh28i7gb00kx01qybd5l5mwx',
+              initialCameraPosition: CameraPosition(
+                target: LatLng(latitude, longitude),
+                zoom: 14.0,
               ),
-        child: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Current Temperature:',
-              ),
-              Text(
-                '$currenttemp',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
+            ),
           ),
-        ),
+
+          Positioned.fill(
+            child: Center(
+              // Center is a layout widget. It takes a single child and positions it
+              // in the middle of the parent.
+              child: Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Invoke "debug painting" (press "p" in the console, choose the
+                // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                // to see the wireframe for each widget.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'Current Temperature:',
+                  ),
+                  Text(
+                    '$currenttemp',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: manageweather,
